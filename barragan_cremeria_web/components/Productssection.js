1cf6e6a811f6596fca96415bebf241ad23a4d3a8
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-02-27 03:56:29 -0600

    ver 1.2

---
+++ barragan_cremeria_web/components/Productssection.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Productssection() {
+    return (
+        <div className=" min-w-screen relative overflow-hidden flex flex-col items-center ">
+            <div className="relative w-8/12 p-6 mt-4 text-center">
+                <p class="tracking-tight font-display font-bold text-6xl text-slate-100">Nuestros productos</p>
+
+            </div>
+        </div>
+    )
+};
+
